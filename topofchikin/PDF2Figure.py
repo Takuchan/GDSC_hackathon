c338a8d9c420eef76409f85ac3b2cd9726f04592
takuchan <takuchanapp@gmail.com>
2023-11-23 15:28:22 +0900

    シェル領域でファイル名を追加したら処理がされるように変更しました

--- topofchikin/PDF2Figure.py
+++ topofchikin/PDF2Figure.py
@@ -1,17 +1,34 @@
 # -*- coding: utf-8 -*-
 import fitz # pymupdfライブラリ
+import sys
+import os
 
-pdf = 'main.pdf'
-doc = fitz.open(pdf)
 
-file = f'{pdf}_to_text.txt'
-f = open(file, 'w',  encoding='utf-8', errors='ignore')
+def main():
+    pdfs = sys.argv
 
-for page in range(len(doc)):
-    tmp = doc[page].get_text()
-    tmp2 = tmp.encode('utf-8','ignore')
-    text = tmp2.decode('utf-8')
-    f.write(text)
+    
+    if len(pdfs) == 1:
+        print('引数にPDFファイルを指定してください')
+    else:
+        for pdf in pdfs[1:]:
+            if pdf.endswith('.pdf'):
+                doc = fitz.open(pdf)
+                file = f'{pdf}_to_text.txt'
+                f = open(file, 'w',  encoding='utf-8', errors='ignore')
 
-doc.close()
-f.close() 
+                for page in range(len(doc)):
+                    tmp = doc[page].get_text()
+                    tmp2 = tmp.encode('utf-8','ignore')
+                    text = tmp2.decode('utf-8')
+                    f.write(text)
+
+                doc.close()
+                f.close() 
+                print(f'{pdf}の処理が完了しました')
+            else:
+                print(f'{pdf}はPDFファイルではありません。この処理はスキップされました。')
+        
+
+if __name__ == '__main__':
+    main()
