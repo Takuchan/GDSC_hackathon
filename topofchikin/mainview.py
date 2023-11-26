15c39e88c93dbadda5244620e32fef54247e5db5
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-11-26 18:07:42 +0900

    feat: ファイル選択機能を追加

--- topofchikin/mainview.py
+++ topofchikin/mainview.py
@@ -1,18 +1,44 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import filedialog
 
-root = Tk()
-root.title("sazameki Mover")
+class MyGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("sazameki Mover")
+        master.geometry("400x200")
 
-# ラベル設定
-choseLabel = Label(root, text="ファイルを選択してください。")
+        self.file_label = tk.Label(master, text="選択したファイル: ")
+        self.file_label.pack()
 
-# ボタン設定
-button1 = Button(root, text="ファイル選択", command={})
-button2 = Button(root, text="分類開始", command={})
+        self.select_button = tk.Button(master, text="ファイルを選択", command=self.select_file)
+        self.select_button.pack()
 
-# 配置
-choseLabel.pack()
-button1.pack()
-button2.pack()
+        self.complete_button = tk.Button(master, text="完了", command=self.show_selected_file)
+        self.complete_button.pack()
 
-root.mainloop()
+        self.classify_button = tk.Button(master, text="分類", command=self.run_classification)
+        self.classify_button.pack()
+
+    def select_file(self):
+        file_path = filedialog.askopenfilename()
+        # 選択したファイルに対する処理をここに追加
+        self.selected_file = file_path
+
+    def show_selected_file(self):
+        try:
+            file_name = self.selected_file
+            self.file_label.config(text=f"選択したファイル: {file_name}")
+        except AttributeError:
+            self.file_label.config(text="ファイルが選択されていません")
+
+    def run_classification(self):
+        try:
+            # 分類に関するPythonコードをここに追加
+            print(f"分類ボタンが押されました。選択したファイル: {self.selected_file}")
+        except AttributeError:
+            print("ファイルが選択されていません")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    my_gui = MyGUI(root)
+    root.mainloop()
