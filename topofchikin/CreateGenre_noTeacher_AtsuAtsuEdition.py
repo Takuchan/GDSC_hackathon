b7a3b1baf84edf0cafb2abe5cea627aa4287c5a2
atsuatsu0120 <masa_yuka_atu0120@yahoo.co.jp>
2023-11-26 16:24:29 +0900

    feat: 教師なし学習

---
+++ topofchikin/CreateGenre_noTeacher_AtsuAtsuEdition.py
@@ -0,0 +1,32 @@
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+import os
+
+n_clusters_input = int(input("クラスター数を指定してください。"))
+
+# ファイルのリストを取得します
+folder_path = 'topofchikin/datas'  # 'your_folder'をあなたのフォルダパスに置き換えてください
+file_names = os.listdir(folder_path)
+
+# 各ファイルのテキストを読み込みます
+texts = []
+for file_name in file_names:
+    with open(os.path.join(folder_path, file_name), 'r') as f:
+        texts.append(f.read())
+
+# TF-IDFベクトル化を行います
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(texts)
+
+# KMeansオブジェクトを作成します
+kmeans = KMeans(n_clusters=n_clusters_input)
+
+# モデルをトレーニングします
+kmeans.fit(X)
+
+# 各データポイントのクラスタラベルを取得します
+labels = kmeans.labels_
+
+# 各ファイル名と対応するラベルを出力します
+for file_name, label in zip(file_names, labels):
+    print(f"File: {file_name}, Label: {label}")
