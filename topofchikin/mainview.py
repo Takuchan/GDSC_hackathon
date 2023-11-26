a3a9672fea4700b29690d3277679503d736ce58c
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-11-26 17:59:23 +0900

    feat: ボタントラベルを設置

--- topofchikin/mainview.py
+++ topofchikin/mainview.py
@@ -1,4 +1,18 @@
 from tkinter import *
 
 root = Tk()
+root.title("sazameki Mover")
+
+# ラベル設定
+choseLabel = Label(root, text="ファイルを選択してください。")
+
+# ボタン設定
+button1 = Button(root, text="ファイル選択", command={})
+button2 = Button(root, text="分類開始", command={})
+
+# 配置
+choseLabel.pack()
+button1.pack()
+button2.pack()
+
 root.mainloop()
