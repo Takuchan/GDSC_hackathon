78352bda51a535e4910a2564999359a04f2cbe02
nyaga <c1223781@st.kanazawa-it.ac.jp>
2023-11-26 18:11:10 +0900

    feat: 複数選択機能を追加

--- topofchikin/mainview.py
+++ topofchikin/mainview.py
@@ -10,31 +10,33 @@ class MyGUI:
         self.file_label = tk.Label(master, text="選択したファイル: ")
         self.file_label.pack()
 
-        self.select_button = tk.Button(master, text="ファイルを選択", command=self.select_file)
+        self.select_button = tk.Button(master, text="ファイルを選択", command=self.select_files)
         self.select_button.pack()
 
-        self.complete_button = tk.Button(master, text="完了", command=self.show_selected_file)
+        self.complete_button = tk.Button(master, text="完了", command=self.show_selected_files)
         self.complete_button.pack()
 
         self.classify_button = tk.Button(master, text="分類", command=self.run_classification)
         self.classify_button.pack()
 
-    def select_file(self):
-        file_path = filedialog.askopenfilename()
-        # 選択したファイルに対する処理をここに追加
-        self.selected_file = file_path
+        self.selected_files = []
 
-    def show_selected_file(self):
+    def select_files(self):
+        files = filedialog.askopenfilenames()
+        # 選択したファイルに対する処理をここに追加
+        self.selected_files = files
+
+    def show_selected_files(self):
         try:
-            file_name = self.selected_file
-            self.file_label.config(text=f"選択したファイル: {file_name}")
+            file_names = "\n".join(self.selected_files)
+            self.file_label.config(text=f"選択したファイル:\n{file_names}")
         except AttributeError:
             self.file_label.config(text="ファイルが選択されていません")
 
     def run_classification(self):
         try:
             # 分類に関するPythonコードをここに追加
-            print(f"分類ボタンが押されました。選択したファイル: {self.selected_file}")
+            print(f"分類ボタンが押されました。選択したファイル: {self.selected_files}")
         except AttributeError:
             print("ファイルが選択されていません")
 
