88386a454bfa90e74a622f2fd9bbaf387d774740
Takuchan <takuchanapp@gmail.com>
2023-12-02 23:18:15 +0900

    タイトル生成

--- MainProgram/process.py
+++ MainProgram/process.py
@@ -1,6 +1,8 @@
+# coding: UTF-8
+
 import topofchikin.PDF2Figure as p2f
 import topofchikin.CreateGenre_noTeacher_AtsuAtsuEdition as cgn2
-
+import topofchikin.GenerateTitle as generateTitle
 import sys
 
 def main():
@@ -10,6 +12,10 @@ def main():
     while (pdfs.getProcessed() == False):
         print("処置中です。")
     cgn2
+    title = generateTitle.GenerateTitle().process(
+    "私は、その男の写真を三葉、見たことがある。一葉は、その男の、幼年時代、とでも言うべきであろうか、十歳前後かと推定される頃の写真であって、その子供が大勢の女のひとに取りかこまれ、"
+    )
+    print(title)
     
 
 
--- MainProgram/topofchikin/GenerateTitle.py
+++ MainProgram/topofchikin/GenerateTitle.py
@@ -1,21 +1,77 @@
 import google.generativeai as palm
+# import vertexai
+# from vertexai.language_models import TextGenerationModel
+from google.cloud import translate_v2 as translate
+import os 
 
-API_KEY = ''
-palm.configure(api_key=API_KEY)
 
-model_list = [_ for _ in palm.list_models()]
-for model in model_list:
-    print(model.name)
 
-model_id = 'models/text-bison-001'
-prompt = '''
-I am going on a date on Friday with a boy I like. What should I wear?
-'''
-completion = palm.generate_text(
-    model=model_id,
-    prompt=prompt,
-    temperature=0.8,
-    max_output_tokens=800,
-)
+class GenerateTitle:
+    def __init__(self):
+        self.API_KEY = ''
+        pass
 
-print(completion.result)
+
+    def translate_text2jp(self,text):
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ""
+        translate_client = translate.Client()
+        result = translate_client.translate(text, target_language="ja")
+
+        return result["translatedText"]
+
+    def translate_text2en(self,text):
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ""
+        translate_client = translate.Client()
+        result = translate_client.translate(text, target_language="en")
+        # print("Text: {}".format(result["input"]))
+        # print("Translation: {}".format(result["translatedText"]))
+        return result["translatedText"]
+
+    def process(self,text):
+        palm.configure(api_key=self.API_KEY)
+        model_id = 'models/text-bison-001'
+        engText = self.translate_text2en(text)
+        prompt = '''Read the following sentence and make it a 10-word title.
+        
+        '''+ engText
+
+        completion = palm.generate_text(
+            model=model_id,
+            prompt=prompt,
+            temperature=0.8,
+            max_output_tokens=800,
+        )
+
+
+        return self.translate_text2jp(completion.result)
+    
+
+
+
+# model_list = [_ for _ in palm.list_models()]
+# for model in model_list:
+#     print(model.name)
+
+
+
+# class GenerateTitle:
+#     def __init__(self):
+#         pass
+
+#     def generate_title(self, text):
+#         vertexai.init(project="gdschackathon2023test", location="us-central1")
+#         parameters = {
+#             "candidate_count": 1,
+#             "max_output_tokens": 1024,
+#             "temperature": 0.2,
+#             "top_p": 0.8,
+#             "top_k": 40
+#         }
+#         model = TextGenerationModel.from_pretrained("text-bison")
+#         response = model.predict(
+#             """以下の文章を10文字程度のタイトルにしてください。 {}""".format(text),
+#             **parameters
+#         )
+#         print(f"Response from Model: {response.text}")
+#         return response.text
+
